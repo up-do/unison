683cb40eb7f768deff396f9404747008dcdbb928
Phil de Joux <philderbeast@gmail.com>
2023-12-06 13:51:46 -0500

    Generate projects

---
+++ project-nix/ghc-9.2.8/sha256map.nix
@@ -0,0 +1,4 @@
+{
+  "https://github.com/unisonweb/configurator"."e47e9e9fe1f576f8c835183b9def52d73c01327a" = "10jripksd08ni6zxx7d06ipbf8xmqmnzvz3zxbk2bkman5c9sypw";
+  "https://github.com/unisonweb/haskeline"."9275eea7982dabbf47be2ba078ced669ae7ef3d5" = "04r4h3bf6sdmli17al2d07rascmi2hman15vhw71y20fnhhiaaga";
+}
